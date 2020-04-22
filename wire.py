19582186dbeb4b4fb353197e6628458abee6c80b
shivampdesai <shivampdesai@yahoo.com>
2020-04-21 19:29:40 -0700

    added comments to wire

--- wire.py
+++ wire.py
@@ -8,6 +8,7 @@ from gui_views.dialogs import *
 class Wire(QGraphicsLineItem):
     def __init__(self, line, pen, state):
         super(Wire, self).__init__()
+        # set pen and line passed in
         self.setPen(pen)
         self.setLine(line)
         self.parent_key = None
@@ -15,17 +16,24 @@ class Wire(QGraphicsLineItem):
         self.state = state
 
     def mousePressEvent(self, event):
+        """register mouse press events"""
+
         parent = self.state.sym_objects[self.child_key[1]]
         child = self.state.sym_objects[self.parent_key[1]]
 
+        # confirm deletion
         dialog = deleteWireDialog("Delete connection between " + parent.name +
                                 "." + self.parent_key[2] + " and " + child.name
                                 + "." + self.parent_key[3] + "?")
 
+        # if yes, delete
         if dialog.exec_():
             self.deleteWire()
 
     def deleteWire(self):
+        """delete all backend entries associate with connection and remove
+        from scene"""
+
         parent = self.state.sym_objects[self.child_key[1]]
         child = self.state.sym_objects[self.parent_key[1]]
         parent.instance_ports[self.parent_key[2]]['Value'] = None
