921c47a8455d2cba95efdd43537f8df1dcee59ae
shivampdesai <shivampdesai@yahoo.com>
2020-04-28 18:31:17 -0700

    added arrows to wires

--- wire.py
+++ wire.py
@@ -5,15 +5,35 @@ from PySide2.QtGui import *
 
 from gui_views.dialogs import *
 
-class Wire(QGraphicsLineItem):
+import math
+
+class Wire(QGraphicsItemGroup):
     def __init__(self, line, pen, state):
         super(Wire, self).__init__()
-        # set pen and line passed in
-        self.setPen(pen)
-        self.setLine(line)
+        # set line passed in
+        graphicsLine = QGraphicsLineItem(line)
+        graphicsLine.setPen(pen)
         self.parent_key = None
         self.child_key = None
         self.state = state
+
+        endpoint_x = line.x2()
+        endpoint_y = line.y2()
+
+        # create both lines for arrowhead
+        arrowhead1 = QLineF(endpoint_x, endpoint_y, endpoint_x + 5, endpoint_y + 5)
+        arrowhead1.setAngle(line.angle() + 135)
+        arrow1 = QGraphicsLineItem(arrowhead1)
+        arrow1.setPen(pen)
+
+        arrowhead2 = QLineF(endpoint_x, endpoint_y, endpoint_x + 5, endpoint_y + 5)
+        arrowhead2.setAngle(line.angle() - 135)
+        arrow2 = QGraphicsLineItem(arrowhead2)
+        arrow2.setPen(pen)
+
+        self.addToGroup(graphicsLine)
+        self.addToGroup(arrow1)
+        self.addToGroup(arrow2)
 
         # line only recieves double click events if it is selectable
         self.setFlag(QGraphicsItem.ItemIsSelectable, True)
@@ -21,7 +41,7 @@ class Wire(QGraphicsLineItem):
     def mouseDoubleClickEvent(self, event):
         """register mouse press events"""
 
-        # remove bounding box 
+        # remove bounding box
         self.setSelected(False)
 
         parent = self.state.sym_objects[self.child_key[1]]
