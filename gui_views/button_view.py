c61484a19c3fe43cb5a55818a178dda33a980321
Parallels <ravishdeep10@gmail.com>
2020-05-02 15:35:14 -0700

    temp fix for bug when importing outside of curr dir

--- gui_views/button_view.py
+++ gui_views/button_view.py
@@ -135,6 +135,8 @@ class ButtonView(): #export, draw line, save and load self.stateuration buttons
                 '',"python files (*.py)")[0]
 
             tokens = full_path.split('/')
+            dir_path = '/'.join(tokens[:len(tokens) - 1]) + '/'
+            sys.path.append(dir_path)
             module_name = tokens[len(tokens) - 1].split('.')[0]
             import_module(module_name, package=full_path)
             clsmembers = inspect.getmembers(sys.modules[module_name], \
